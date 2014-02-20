ce996a068bae969e5ecf478100dac29cbf239304
Per Sandstrom <pg.sandstrom@gmail.com>
2014-02-20 20:44:28 +0100

    added som js

--- functions.php
+++ functions.php
@@ -81,9 +81,13 @@ add_action( 'widgets_init', 'test123123_widgets_init' );
 function test123123_scripts() {
 	wp_enqueue_style( 'test123123-style', get_stylesheet_uri() );
 
-	wp_enqueue_script( 'test123123-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
+    wp_enqueue_script( 'test123123-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
 
 	wp_enqueue_script( 'test123123-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
+
+    wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/js/bootstrap.js', array(), '20120206', true );
+    wp_enqueue_script( 'customizer', get_template_directory_uri() . '/js/customizer.js', array(), '20120206', true );
+    wp_enqueue_script( 'jquery', get_template_directory_uri() . '/js/jquery-1.11.0.js', array(), '20120206', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
