e2b6884b5b459ee19a60749bcde0bf7eb52bc001
Per Sandstrom <pg.sandstrom@gmail.com>
2014-02-20 21:29:34 +0100

    css experimenting

--- header.php
+++ header.php
@@ -9,32 +9,38 @@
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-<meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<title><?php wp_title( '|', true, 'right' ); ?></title>
-<link rel="profile" href="http://gmpg.org/xfn/11">
-<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-    <link href="/css/bootstrap-theme.css" rel="stylesheet">
-<link href="/css/bootstrap.css" rel="stylesheet">
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title><?php wp_title('|', true, 'right'); ?></title>
+    <link rel="profile" href="http://gmpg.org/xfn/11">
+    <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
+    <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/bootstrap-theme.css" type="text/css"
+          media="screen"/>
+    <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/bootstrap.css" type="text/css"
+          media="screen"/>
 
-<?php wp_head(); ?>
+    <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
 <div id="page" class="hfeed site">
 
-	<header id="masthead" class="site-header" role="banner">
-		<div class="site-branding">
-			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
-		</div>
+    <header id="masthead" class="site-header" role="banner">
+        <div class="site-branding">
+            <h1 class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>"
+                                      rel="home"><?php bloginfo('name'); ?></a></h1>
 
-		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<h1 class="menu-toggle"><?php _e( 'Menu', 'test123123' ); ?></h1>
-			<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'test123123' ); ?></a>
+            <h2 class="site-description"><?php bloginfo('description'); ?></h2>
+        </div>
 
-			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+        <nav id="site-navigation" class="main-navigation" role="navigation">
+            <h1 class="menu-toggle"><?php _e('Menu', 'test123123'); ?></h1>
+            <a class="skip-link screen-reader-text" href="#content"><?php _e('Skip to content', 'test123123'); ?></a>
 
-	<div id="content" class="site-content">
+            <?php wp_nav_menu(array('theme_location' => 'primary')); ?>
+        </nav>
+        <!-- #site-navigation -->
+    </header>
+    <!-- #masthead -->
+
+    <div id="content" class="site-content">
--- index.php
+++ index.php
@@ -15,7 +15,7 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 	HEJ
-		<main id="main" class="site-main" role="main">
+		<main id="main" class="site-main col-md-4" role="main">
 
 		<?php if ( have_posts() ) : ?>
 
