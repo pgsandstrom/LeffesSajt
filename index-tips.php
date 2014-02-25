6cbcfaa54a50c0e2a2d91eefc56c0c1b277d219d
Per Sandstrom <pg.sandstrom@gmail.com>
2014-02-25 22:53:33 +0100

    fixed pagination in tips

--- index-tips.php
+++ index-tips.php
@@ -10,39 +10,25 @@ get_header(); ?>
     <?php get_sidebar('left'); ?>
     <main id="main" class="site-main col-md-6 col-sm-8 col-xs-12" role="main">
 
-
         <?php
         $args = array(
             'category_name' => 'tips',
-            'paged' => 1,
+            'paged' => $paged,
             'posts_per_page' => 5
         );
-        ?>
-
-<!--        'category_name=tips'-->
-        <?php $my_query = new WP_Query($args); ?>
-        <?php if ($my_query->have_posts()) : ?>
-
-            <?php while ($my_query->have_posts()) : $my_query->the_post(); ?>
-                <?php
+        $wp_query = new WP_Query($args);
+        if ($wp_query->have_posts()) :
+            while ($wp_query->have_posts()) : $wp_query->the_post();
                 /* Include the Post-Format-specific template for the content.
                  * If you want to override this in a child theme, then include a file
                  * called content-___.php (where ___ is the Post Format name) and that will be used instead.
                  */
                 get_template_part('content', get_post_format());
-//					the_title();
-
-                ?>
-
-            <?php endwhile; ?>
-
-            <?php test123123_paging_nav(); ?>
-
-        <?php else : ?>
-
-            <?php get_template_part('content', 'none'); ?>
-
-        <?php endif; ?>
+            endwhile;
+            test123123_paging_nav(); //
+        else :
+            get_template_part('content', 'none');
+        endif; ?>
 
     </main>
     <!-- #main -->
