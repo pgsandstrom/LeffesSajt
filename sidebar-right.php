6d3b89c4d8aee8fc8c40d0a29cb955d12fa072d0
Per Sandstrom <pg.sandstrom@gmail.com>
2014-02-26 20:52:07 +0100

    sidebar right started

--- sidebar-right.php
+++ sidebar-right.php
@@ -6,6 +6,10 @@
  */
 ?>
 	<div id="secondary" class="widget-area sidebar-right col-md-3 hidden-sm" role="complementary">
-        RIGHT SIDEBAR
-
+        <aside id="phone" class="widget">
+            <div id="phone-float-title">Tips om innovation i mobilen varje dag!</div>
+            <div id="phone-title">Dagens tips</div>
+            <img id="phone-seperator" src="<?php bloginfo('template_directory'); ?>/img/phone_line.png" alt=""/>
+            <div id="phone-body">Lite statisk text som ligger här under tiden, du vet.</div>
+        </aside>
 	</div><!-- #secondary -->
