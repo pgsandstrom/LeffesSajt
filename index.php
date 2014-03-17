047080dcabd15bcaf0f555976fb80f979f7ca0fd
Per Sandstrom <pg.sandstrom@gmail.com>
2014-03-17 21:36:34 +0100

    fixed a bug in linking to category

--- index.php
+++ index.php
@@ -45,15 +45,15 @@ get_header(); ?>
     <main id="main" class="site-main col-md-6 col-sm-8 col-xs-12" role="main">
 
         <?php
-        $args = array(
-            'category_name' => 'artiklar',
-            'paged' => $paged
-//        ,
-//            'posts_per_page' => 5
-        );
-        $wp_query = new WP_Query($args);
-        if ($wp_query->have_posts()) :
-            while ($wp_query->have_posts()) : $wp_query->the_post();
+        //        $args = array(
+        //            'category_name' => 'artiklar',
+        //            'paged' => $paged
+        //        ,
+        //            'posts_per_page' => 5     <- denna sak inte användas
+        //        );
+        //        $wp_query = new WP_Query($args);
+        if (have_posts()) :
+            while (have_posts()) : the_post();
                 /* Include the Post-Format-specific template for the content.
                  * If you want to override this in a child theme, then include a file
                  * called content-___.php (where ___ is the Post Format name) and that will be used instead.
