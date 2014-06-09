63b18d5c0e7b1a7d83dfcd3dc1bd88c795fa8f68
Per Sandstrom <pg.sandstrom@gmail.com>
2014-06-09 21:34:47 +0200

    fixed new app-sidebar

--- sidebar-right.php
+++ sidebar-right.php
@@ -7,16 +7,23 @@
 ?>
 <div id="sidebar-right" class="widget-area sidebar sidebar-right col-md-3 hidden-sm hidden-xs" role="complementary">
 
-    <aside  class="twitter-follow">
-        <a href="https://twitter.com/tusentips" class="twitter-follow-button" data-show-count="false" data-lang="sv" data-size="large">Följ @tusentips</a>
-    </aside>
+	<aside class="twitter-follow">
+		<a href="https://twitter.com/tusentips" class="twitter-follow-button" data-show-count="false" data-lang="sv" data-size="large">Följ @tusentips</a>
+	</aside>
 
-    <aside  class="">
-        <div>Snart som app</div>
-        <div>i din mobil!</div>
-        <!--        <a href="http://vinnova.se" target="_blank">-->
-        <img id="vinnova" src="<?php echo get_bloginfo('template_directory'); ?>/img/appunderconstruction-bw.png"/>
-        <!--        </a>-->
-    </aside>
+	<aside id="app-content">
+		<div class="text">Nu som app</div>
+		<div class="text">i din mobil!</div>
+		<img src="<?php echo get_bloginfo('template_directory'); ?>/img/app_screenshot.png"/>
+
+		<div id="appstores">
+			<a href="https://itunes.apple.com/se/app/tusentips/id877286390?mt=8&uo=4" target="itunes_store">
+				<img src="<?php echo get_bloginfo('template_directory'); ?>/img/appstore.png" class="center"/>
+			</a>
+			<a href="https://play.google.com/store/apps/details?id=se.tusentips.app" target="_blank">
+				<img src="<?php echo get_bloginfo('template_directory'); ?>/img/playstore.png" class="center"/>
+			</a>
+		</div>
+	</aside>
 
 </div><!-- #sidebar-right -->
